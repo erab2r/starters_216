89048b6f0670381adc39c472c7d371a4d1d0c0e3
erab2r <shkera213@gmail.com>
2025-12-11 22:07:02 +0600

    1

---
+++ LIS_AND_LDS.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc;
+    cin >> tc;
+
+    while (tc--) {
+        int N;
+        cin >> N;
+        int anss = 0; 
+        for (int i = 1; i <= N; i++) {
+            int  e;
+            cin >> e;
+            if (e == 1) 
+                anss++;
+        }
+
+        int ress = N - anss / 2;
+        cout << ress << "\n";
+    }
+
+    return 0;
+}
