1e16b7a28df8c536a22155541c940ce130adf65d
erab2r <shkera213@gmail.com>
2025-12-17 01:53:15 +0600

    1

--- LIS_AND_LDS.cpp
+++ LIS_AND_LDS.cpp
@@ -2,24 +2,31 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int tc;
-    cin >> tc;
-
-    while (tc--) {
-        int N;
-        cin >> N;
-        int anss = 0; 
-        for (int i = 1; i <= N; i++) {
-            int  e;
-            cin >> e;
-            if (e == 1) 
-                anss++;
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n,0);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
         }
-        int ress = N - anss / 2;
-        cout << ress << "\n";
+
+        int c1 = 0;  
+        int c2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                c1++;
+            } else if (arr[i] >= 2) {
+                c2++;
+            }
+        }
+
+        // ans = c2 + ceil(c1 / 2)
+        int ans = c2 + (c1 + 1) / 2;
+
+        cout << ans << endl;
     }
-    return 0;
 }
