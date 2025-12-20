e4691c90b6a97486995ad4dbfaea1b5f37c0f185
erab2r <shkera213@gmail.com>
2025-12-20 23:33:38 +0600

    1

--- p1.cpp
+++ p1.cpp
@@ -1,212 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-static const int LOG = 20;
-int prnt[200005][LOG];
-int prnt_Edge[200005];
-int dpth[200005];
-int e_in[200005], e_out[200005];
-vector<pair<int,int>> adj[200005];
-int timer;
-int N;
-void dfs_rt(int root){
-    stack<pair<int,int>> st;
-    st.push({root,0});
-    prnt[root][0] = root;
-    dpth[root] = 0;
-    prnt_Edge[root] = -1;
-    while(!st.empty()){
-        int u = st.top().first;
-        int e = st.top().second;
-        st.pop();
-        for(size_t i=0;i<adj[u].size();i++){
-            int h = adj[u][i].first;
-            int he = adj[u][i].second;
-            if(h == e) 
-                continue;
-            dpth[h] = dpth[u] + 1;
-            prnt[h][0] = u;
-            prnt_Edge[h] = he;
-            st.push({h,u});
-        }
-    }
-}
-void dfs_fun(int root){
-    timer = 0;
-    vector<pair<int,int>> stkk;
-    stkk.push_back({root,0});
-    while(!stkk.empty()){
-        int u = stkk.back().first;
-        int &state = stkk.back().second;
-        if(state == 0){
-           e_in[u] = timer++;
-        }
-        if(state < (int)adj[u].size()){
-            int v = adj[u][state].first;
-            int p = prnt[u][0];
-            state++;
-            if(v == p) 
-                continue;
-            stkk.push_back({v,0});
-        } else {
-            e_out[u] = timer;
-            stkk.pop_back();
-        }
-    }
-}
-int lcA(int a, int b){
-    if(dpth[a] < dpth[b]) 
-        swap(a,b);
-    int diff = dpth[a]-dpth[b];
-    for(int k=0;k<LOG;k++){
-        if(diff & (1<<k)) a = prnt[a][k];
-    }
-    if(a == b) 
-        return a;
-    for(int k=LOG-1;k>=0;k--){
-        if(prnt[a][k] != prnt[b][k]){
-            a = prnt[a][k];
-            b = prnt[b][k];
-        }
-    }
-    return prnt[a][0];
-}
-
-inline int dist(int a, int b){
-    int c = lcA(a,b);
-    return dpth[a] + dpth[b] - 2*dpth[c];
-}
-inline bool inSub(int u, int v){
-    return e_in[u] >= e_in[v] && e_in[u] <= e_out[v];
-}
-
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T; 
-    cin >> T;
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        int k;
+        cin >> n >> k;
 
-    while(T--){
-        cin >> N;
-        int M = N + 1;
-
-        vector<pair<int,int>> edges(M);
-        for(int i=1;i<=N;i++){
-            adj[i].clear();
-        }
-
-        for(int i=0;i<M;i++){
-            cin >> edges[i].first >> edges[i].second;
-        }
-        vector<int> dsu(N+1);
-        for(int i=1;i<=N;i++) dsu[i]=i;
-
-        function<int(int)> fp = [&](int x){
-            return dsu[x]==x?x:dsu[x]=fp(dsu[x]);
-        };
-
-        vector<int> isTree(M,0);
-        vector<int> backEdges;
-        for(int i=0;i<M;i++){
-            int u = edges[i].first, v = edges[i].second;
-            int ru = fp(u), rv = fp(v);
-            if(ru != rv){
-                dsu[rv] = ru;
-                isTree[i] = 1;
-                adj[u].push_back({v,i});
-                adj[v].push_back({u,i});
-            } else {
-                backEdges.push_back(i);
-            }
-        }
-        int R = fp(1);
-        bool ok = true;
-        for(int i=1;i<=N;i++){
-            if(fp(i) != R){ ok=false; break; }
-        }
-        if(!ok){
-            cout << 0 << "\n";
+        if (k == 1) {
+            cout << n << "\n";
             continue;
         }
-        if(backEdges.size() != 2){
-            cout << 0 << "\n";
-            continue;
-        }
-        dfs_rt(1);
-        for(int k=1;k<LOG;k++){
-            for(int v=1;v<=N;v++){
-                prnt[v][k] = prnt[prnt[v][k-1]][k-1];
+
+        if (k % 2 == 1) {
+            // odd k → all n
+            for (int i = 0; i < k; i++) {
+                cout << n << " ";
             }
-        }
-        dfs_fun(1);
-        static int treeChild[200005];
-        for(int i=0;i<M;i++) 
-            treeChild[i] = -1;
-
-        for(int v=2; v<=N; v++){
-            int eid = prnt_Edge[v];
-            if(eid != -1){
-                treeChild[eid] = v;
+            cout << "\n";
+        } else {
+            // even k → one 0, rest n
+            cout << 0 << " ";
+            for (int i = 1; i < k; i++) {
+                cout << n << " ";
             }
+            cout << "\n";
         }
-
-        int be1 = backEdges[0], be2 = backEdges[1];
-        int x1 = edges[be1].first, y1 = edges[be1].second;
-        int x2 = edges[be2].first, y2 = edges[be2].second;
-
-        int len1 = dist(x1,y1) + 1;
-        int len2 = dist(x2,y2) + 1;
-
-        static int cntOn[200005];
-        for(int i=0;i<M;i++) cntOn[i]=0;
-
-        for(int i=0;i<M;i++){
-            if(!isTree[i]) continue;
-            int child = treeChild[i];
-            if(child == -1) continue;
-
-            int c = 0;
-            if(inSub(x1,child) ^ inSub(y1,child)) c++;
-            if(inSub(x2,child) ^ inSub(y2,child)) c++;
-            cntOn[i] = c;
-        }
-
-        long long anss = 0;
-
-        for(int i=0;i<M;i++){
-            if(!isTree[i]){
-                if(i == be1) 
-                    anss += len2;
-                else anss += len1;
-            } else {
-                int c = cntOn[i];
-                int child = treeChild[i];
-                if(c == 1){
-                    bool inc1 = (inSub(x1,child) ^ inSub(y1,child));
-                    if(inc1) 
-                        anss += len2;
-                    else anss += len1;
-                }
-                else if(c == 2){
-                    int s1,o1,s2,o2;
-                    if(inSub(x1,child)){ 
-                        s1=x1; o1=y1; 
-                    } 
-                    else { 
-                        s1=y1; o1=x1; 
-                    }
-                    if(inSub(x2,child)){ s2=x2; o2=y2; } 
-                    else { s2=y2; o2=x2; }
-
-                    anss += (long long)dist(s1,s2) + dist(o1,o2) + 2LL;
-                }
-                else {
-                }
-            }
-        }
-
-        cout << (anss/2) << "\n";
     }
     return 0;
 }
--- p2.cpp
+++ p2.cpp
@@ -1,54 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long computeScore(const vector<int>& P) {
-    int N = P.size();
-    long long ans = 0;
-
-    for (int L = 0; L < N; L++) {
-        vector<int> freq(N + 1, 0);
-        int mex = 0;
-
-        for (int R = L; R < N; R++) {
-            freq[P[R]] = 1;
-            while (freq[mex]) mex++;
-            ans += mex;
-        }
-    }
-    return ans;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int N;
-        cin >> N;
-        vector<int> Q(N);
+    int t;
+    cin >> t;
+    while (t--) {
+        long long l, r;
+        cin >> l >> r;
 
-        vector<int> present(N, 0);
-        for (int i = 0; i < N; i++) {
-            cin >> Q[i];
-            if (Q[i] != -1) present[Q[i]] = 1;
+        long long d = l ^ r; 
+
+        if (d == 0) {
+            cout << 0 << "\n"; 
+            continue;
         }
+        int k = 63 - __builtin_clzll(d);
 
-        vector<int> missing;
-        for (int x = 0; x < N; x++)
-            if (!present[x]) missing.push_back(x);
-
-        // Sort missing numbers descending
-        sort(missing.rbegin(), missing.rend());
-        int idx = 0;
-        for (int i = 0; i < N; i++) {
-            if (Q[i] == -1) {
-                Q[i] = missing[idx++];
-            }
-        }
-
-        cout << computeScore(Q) << "\n";
+        long long ans = (1LL << k) - 1;
+        cout << ans << "\n";
     }
-    return 0;
 }
